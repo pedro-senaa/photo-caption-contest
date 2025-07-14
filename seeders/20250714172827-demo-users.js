e855818ca13316bbdd7c83c888332dc462159f55
pedro-senaa <pedro.s.correa00@gmail.com>
2025-07-14 15:52:49 -0300

    add seeders and finish setting sequelize

--- seeders/20250714172827-demo-users.js
+++ seeders/20250714172827-demo-users.js
@@ -2,24 +2,23 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+        username: 'nome1',
+        password: 'senha1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, {
+        username: 'nome2',
+        password: 'senha2',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ], {})
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
--- seeders/20250714172844-images-insert.js
+++ seeders/20250714172844-images-insert.js
@@ -2,24 +2,30 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('images', [
+      {
+        // funny cat
+        url: 'https://m.media-amazon.com/images/I/91I89Qh5JzL.png',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        // cute dog
+        url: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        // landscape
+        url: 'https://www.windowslatest.com/wp-content/uploads/2024/10/Windows-XP-4K-modified-scaled.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('images', null, {})
   }
 };
